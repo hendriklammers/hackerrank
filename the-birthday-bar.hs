733fffaf7fcca41c6f701f49d4b2613415d00c1a
Hendrik Lammers <lammers@gmail.com>
2019-05-11 15:10:46 +0700

    Refactor

--- the-birthday-bar.hs
+++ the-birthday-bar.hs
@@ -1,8 +1,12 @@
 divide :: Int -> Int -> [Int] -> Int
 divide d m s
     | length s < m = 0
-    | sum (take m s) == d = 1 + divide d m (tail s)
-    | otherwise = 0 + divide d m (tail s)
+    | otherwise = summed + divide d m (tail s)
+  where
+    summed =
+        if sum (take m s) == d
+            then 1
+            else 0
 
 solve :: [Int] -> Int
 solve (n:xs) = divide d m s
